d7e6bf084d79b56864ebd13294e92103eedb1910
feng <fly_elephant@163.com>
2015-03-30 16:45:31 +0800

    add splash page

---
+++ app/src/main/java/com/news/yazhidao/pages/SplashAty.java
@@ -0,0 +1,24 @@
+package com.news.yazhidao.pages;
+
+import com.news.yazhidao.R;
+import com.news.yazhidao.common.BaseActivity;
+
+/**
+ * Created by fengjigang on 15/3/30.
+ */
+public class SplashAty extends BaseActivity {
+    @Override
+    protected void setContentView() {
+        setContentView(R.layout.aty_splash);
+    }
+
+    @Override
+    protected void initializeViews() {
+
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+}
