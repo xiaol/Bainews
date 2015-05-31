598b2c188da0ee2fc9cdb5553db2b4219736bfe6
litliang <litliang@126.com>
2015-05-31 18:37:38 +0800

    3 2 grids sure

--- app/src/main/java/com/news/yazhidao/pages/NewsDetailAty.java
+++ app/src/main/java/com/news/yazhidao/pages/NewsDetailAty.java
@@ -1,10 +1,13 @@
 package com.news.yazhidao.pages;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.Message;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -37,6 +40,8 @@ import com.news.yazhidao.utils.ToastUtil;
 import com.news.yazhidao.utils.helper.ImageLoaderHelper;
 import com.news.yazhidao.widget.NewsDetailHeaderView;
 import com.news.yazhidao.widget.ProgressWheel;
+import com.news.yazhidao.widget.imagewall.BitmapUtil;
+import com.news.yazhidao.widget.imagewall.ViewWall;
 import com.news.yazhidao.widget.swipebackactivity.SwipeBackActivity;
 import com.news.yazhidao.widget.swipebackactivity.SwipeBackLayout;
 import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
@@ -44,6 +49,12 @@ import com.umeng.analytics.MobclickAgent;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import app.base.task.BackRunnable;
+import app.base.task.CallbackRunnable;
+import app.base.task.Compt;
 
 
 public class NewsDetailAty extends SwipeBackActivity {
@@ -76,7 +87,7 @@ public class NewsDetailAty extends SwipeBackActivity {
         mSwipeBackLayout = getSwipeBackLayout();
         mSwipeBackLayout.setEdgeTrackingEnabled(SwipeBackLayout.EDGE_LEFT);
 
-        mSource =getIntent().getStringExtra(NewsFeedFragment.KEY_NEWS_SOURCE);
+        mSource = getIntent().getStringExtra(NewsFeedFragment.KEY_NEWS_SOURCE);
         mNewsDetailAdapter = new StaggeredNewsDetailAdapter(this);
         headerView = new NewsDetailHeaderView(this);
 
@@ -101,8 +112,8 @@ public class NewsDetailAty extends SwipeBackActivity {
         _Request.setCallback(new JsonCallback<NewsDetail>() {
 
             @Override
-            public void success(NewsDetail result) {
-                if(result != null) {
+            public void success(final NewsDetail result) {
+                if (result != null) {
                     headerView.setDetailData(result, new NewsDetailHeaderView.HeaderVeiwPullUpListener() {
                         @Override
                         public void onclickPullUp(int height) {
@@ -115,7 +126,49 @@ public class NewsDetailAty extends SwipeBackActivity {
                     if (NewsFeedFragment.VALUE_NEWS_SOURCE.equals(mSource)) {
                         msgvNewsDetail.setSelection(1);
                     }
-                }else{
+                    new Compt().putTask(new BackRunnable() {
+                        @Override
+                        public void run() {
+                            for (Map<String, String> m : result.imgWall) {
+                                String url = m.get("img").toString();
+
+                                BitmapFactory.Options op = null;
+                                while (op == null)
+                                    try {
+                                        op = BitmapUtil.getBitmapFactoryOptions(url);
+                                    } catch (Exception e) {
+                                        // TODO Auto-generated catch block
+                                        continue;
+                                    }
+
+
+                                m.put("w", "" + op.outWidth);
+                                m.put("h", "" + op.outHeight);
+
+
+                            }
+
+
+                            List<HashMap<String, String>> resultList = new ArrayList<HashMap<String, String>>(result.imgWall);
+                            List<HashMap<String, String>> minor = new ArrayList<HashMap<String, String>>();
+                            get3Matched(resultList, minor);
+                            get2Matched(resultList, minor);
+                            get1Matched(resultList, minor);
+
+
+                        }
+                    }
+                            , new CallbackRunnable() {
+                        @Override
+                        public boolean run(Message message, boolean b, Activity activity) throws Exception {
+                            return false;
+                        }
+                    }).run();
+
+
+                } else
+
+                {
                     ToastUtil.toastShort("新闻的内容为空，无法打开");
                     NewsDetailAty.this.finish();
                 }
@@ -126,14 +179,208 @@ public class NewsDetailAty extends SwipeBackActivity {
 
             }
 
+            private List<HashMap<String, String>> get1Matched(List<HashMap<String, String>> resultList, List<HashMap<String, String>> minor) {
+                return minor;
+            }
+
+            private List<HashMap<String, String>> get2Matched(List<HashMap<String, String>> maps, List source) {
+                int stepcnst = 50;
+                float ratio = 1f;
+                float constW = 0;
+                for (int i = stepcnst; i < GlobalParams.screenWidth; i += stepcnst) {
+                    if (i == GlobalParams.screenWidth - 1) {
+                        if (ratio == 1f)
+                            break;
+                        ratio -= 0.1;
+
+                        i = 1;
+                        continue;
+                    }
+                    int scaledw1 = i;
+                    int scaledw2 = GlobalParams.screenWidth - scaledw1;
+                    int scaledh1 = 0;
+                    int scaledh2 = 0;
+                    HashMap<String, String> m1 = null;
+                    HashMap<String, String> m2 = null;
+                    List<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>(source);
+
+                    for (HashMap<String, String> first : list) {
+                        int whead = Integer.parseInt(first.get("w").toString());
+                        int hhead = Integer.parseInt(first.get("h").toString());
+
+                        if (scaledh1 == 0) {
+
+                            scaledh1 = hhead * scaledw1 / whead;
+                            m1 = first;
+                        }
+                        List<HashMap<String, String>> list2 = new ArrayList<HashMap<String, String>>(list);
+                        for (HashMap<String, String> m : list2) {
+
+                            int w = Integer.parseInt(m.get("w").toString());
+                            int h = Integer.parseInt(m.get("h").toString());
+
+
+                            scaledh2 = h * scaledw2 / w;
+
+                            if (Math.abs(scaledh1 - scaledh2) < 10) {
+                                if (scaledh2 * ratio >= scaledh1) {
+                                    m2 = m;
+                                    m2.put("units", "2");
+                                    m2.put("position", "2");
+                                    m2.put("scaledh", "" + scaledh2);
+                                    m2.put("scaledw", "" + scaledw2);
+                                    m1.put("units", "2");
+                                    m1.put("position", "1");
+                                    m1.put("scaledh", "" + scaledh1);
+                                    m1.put("scaledw", "" + scaledw1);
+                                    maps.add(m1);
+                                    maps.add(m2);
+                                    source.remove(m2);
+                                    source.remove(m1);
+                                    return source;
+                                } else {
+
+                                    scaledh2 = 0;
+                                    m2 = null;
+                                    continue;
+                                }
+                            }
+
+                        }
+                    }
+                }
+                return source;
+            }
+
+            private List<HashMap<String, String>> get3Matched(List<HashMap<String, String>> maps, List source) {
+                float ratio1 = 1f;
+                float constW = 0;
+                int stepcnst = 50;
+
+                List<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>(source);
+                HashMap<String, String> m1 = null;
+                for (HashMap<String, String> first : list) {
+
+                    int whead = Integer.parseInt(first.get("w").toString());
+                    int hhead = Integer.parseInt(first.get("h").toString());
+                    for (int i = stepcnst; i < GlobalParams.screenWidth; i += stepcnst) {
+                        if (i == GlobalParams.screenWidth - 1) {
+                            if (ratio1 == 1f)
+                                break;
+                            ratio1 -= 0.1;
+
+                            i = 1;
+                            continue;
+                        }
+                        int scaledw1 = i;
+                        int scaledw2 = GlobalParams.screenWidth - scaledw1;
+                        int scaledh1 = 0;
+                        int scaledh2 = 0;
+                        if (scaledh1 == 0) {
+
+                            scaledh1 = hhead * scaledw1 / whead;
+                            m1 = first;
+                        }
+                        List<HashMap<String, String>> list2 = new ArrayList<HashMap<String, String>>(list);
+                        HashMap<String, String> m2 = null;
+                        float ratio2 = 1f;
+                        for (HashMap<String, String> m : list2) {
+
+
+                            int w = Integer.parseInt(m.get("w").toString());
+                            int h = Integer.parseInt(m.get("h").toString());
+
+                            for (int i2 = stepcnst; i2 < GlobalParams.screenWidth; i2 += stepcnst) {
+                                if (i2 == GlobalParams.screenWidth - 1) {
+                                    if (ratio2 == 1f)
+                                        break;
+                                    ratio2 -= 0.1;
+
+                                    i = 1;
+                                    continue;
+                                }
+                                scaledw2 = i;
+                                int scaledw3 = GlobalParams.screenWidth - scaledw1 - scaledw2;
+                                int scaledh3 = 0;
+                                if (scaledh1 == 0) {
+
+                                    scaledh1 = hhead * scaledw1 / whead;
+                                    m1 = first;
+                                }
+                                if (scaledh2 == 0) {
+
+                                    scaledh2 = h * scaledw2 / w;
+                                    m2 = m;
+                                }
+                                scaledh3 = h * scaledw3 / w;
+                                HashMap<String, String> m3 = null;
+                                for (HashMap<String, String> mm : list2) {
+
+
+                                    int w3 = Integer.parseInt(m.get("w").toString());
+                                    int h3 = Integer.parseInt(m.get("h").toString());
+
+
+                                    if (Math.abs(scaledh1 - scaledh2) < 10 && Math.abs(scaledh2 - scaledh3) < stepcnst) {
+                                        if (scaledh2 * ratio1 >= scaledh1 && scaledh3 * ratio2 >= scaledh2) {
+                                            m3 = mm;
+                                            m3.put("units", "3");
+                                            m3.put("position", "3");
+                                            m3.put("scaledh", "" + scaledh3);
+                                            m3.put("scaledw", "" + scaledw3);
+                                            m2.put("units", "3");
+                                            m2.put("position", "2");
+                                            m2.put("scaledh", "" + scaledh2);
+                                            m2.put("scaledw", "" + scaledw2);
+                                            m1.put("units", "3");
+                                            m1.put("position", "1");
+                                            m1.put("scaledh", "" + scaledh1);
+                                            m1.put("scaledw", "" + scaledw1);
+                                            maps.add(m1);
+                                            maps.add(m2);
+                                            maps.add(m3);
+                                            source.remove(m3);
+                                            source.remove(m2);
+                                            source.remove(m1);
+                                            return source;
+                                        } else {
+
+                                            scaledh2 = 0;
+                                            scaledh1 = 0;
+                                            scaledh3 = 0;
+                                            m2 = null;
+                                            m1 = null;
+                                            m3 = null;
+                                            continue;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                }
+                return source;
+            }
+
             @Override
             public void failed(MyAppException exception) {
                 mNewsDetailProgressWheelWrapper.setVisibility(View.GONE);
                 mNewsDetailProgressWheel.stopSpinning();
                 mNewsDetailProgressWheel.setVisibility(View.GONE);
             }
-        }.setReturnType(new TypeToken<NewsDetail>() {
-        }.getType()));
+        }
+
+                .
+
+                        setReturnType(new TypeToken<NewsDetail>() {
+                                }
+
+                                        .
+
+                                                getType()
+
+                        ));
         _Request.execute();
     }
 
@@ -206,8 +453,8 @@ public class NewsDetailAty extends SwipeBackActivity {
     public void finish() {
         super.finish();
         //如果是后台推送新闻消息过来的话，关闭新闻详情页的时候，就会打开主页面
-        if(NewsFeedFragment.VALUE_NEWS_NOTIFICATION.equals(mSource)){
-            Intent intent= new Intent(this,HomeAty.class);
+        if (NewsFeedFragment.VALUE_NEWS_NOTIFICATION.equals(mSource)) {
+            Intent intent = new Intent(this, HomeAty.class);
             startActivity(intent);
         }
     }
