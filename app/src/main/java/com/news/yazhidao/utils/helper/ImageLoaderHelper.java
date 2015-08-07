5fabfc30f1058096272e63f96f0f049aebee0733
ariesy313 <yefei313@126.com>
2015-08-07 19:34:54 +0800

    ariesy 1.0

--- app/src/main/java/com/news/yazhidao/utils/helper/ImageLoaderHelper.java
+++ app/src/main/java/com/news/yazhidao/utils/helper/ImageLoaderHelper.java
@@ -99,8 +99,8 @@ public class ImageLoaderHelper {
         DisplayImageOptions options;
         options = new DisplayImageOptions.Builder()
                 .showImageOnLoading(R.drawable.img_base_big) //设置图片在下载期间显示的图片
-                .showImageForEmptyUri(R.drawable.image_back)//设置图片Uri为空或是错误的时候显示的图片
-                .showImageOnFail(R.drawable.image_back)  //设置图片加载/解码过程中错误时候显示的图片
+                .showImageForEmptyUri(R.drawable.img_base_big)//设置图片Uri为空或是错误的时候显示的图片
+                .showImageOnFail(R.drawable.img_base_big)  //设置图片加载/解码过程中错误时候显示的图片
                 .cacheInMemory(true)//设置下载的图片是否缓存在内存中
                 .cacheOnDisk(true)//设置下载的图片是否缓存在SD卡中
                 .considerExifParams(true)  //是否考虑JPEG图像EXIF参数（旋转，翻转）
