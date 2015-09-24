d3d3f88ef94f282facf8b05379467ec449aeae02
YuanHuiAce <eva1-1@163.com>
2015-09-24 11:03:22 +0800

    shareppo

--- app/src/main/java/com/news/yazhidao/widget/SharePopupWindow.java
+++ app/src/main/java/com/news/yazhidao/widget/SharePopupWindow.java
@@ -66,13 +66,13 @@ public class SharePopupWindow extends PopupWindow {
         //设置SelectPicPopupWindow弹出窗体的宽
         this.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
         //设置SelectPicPopupWindow弹出窗体的高
-        this.setHeight(800);
+        this.setHeight(ViewGroup.LayoutParams.MATCH_PARENT);
         //设置SelectPicPopupWindow弹出窗体可点击
         this.setFocusable(true);
         //设置SelectPicPopupWindow弹出窗体动画效果
         this.setAnimationStyle(R.style.popupWindowAnimation);
         //实例化一个ColorDrawable颜色为半透明
-        ColorDrawable dw = new ColorDrawable(0xb0000000);
+        ColorDrawable dw = new ColorDrawable(0x00000000);
         //设置SelectPicPopupWindow弹出窗体的背景
         this.setBackgroundDrawable(dw);
         //mMenuView添加OnTouchListener监听判断获取触屏位置如果在选择框外面则销毁弹出框
