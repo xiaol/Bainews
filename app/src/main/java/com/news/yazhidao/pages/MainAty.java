844fd7293f693705ee48433db778db7e5161c0c2
feng <fly_elephant@163.com>
2016-02-18 11:31:50 +0800

    stop gps for saving battery

--- app/src/main/java/com/news/yazhidao/pages/MainAty.java
+++ app/src/main/java/com/news/yazhidao/pages/MainAty.java
@@ -71,7 +71,7 @@ public class MainAty extends BaseActivity implements View.OnClickListener, NewsF
 
     private void initLocation(){
         LocationClientOption option = new LocationClientOption();
-        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+        option.setLocationMode(LocationClientOption.LocationMode.Battery_Saving
         );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
         option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
         int span=1000;
@@ -91,7 +91,10 @@ public class MainAty extends BaseActivity implements View.OnClickListener, NewsF
 
         @Override
         public void onReceiveLocation(BDLocation location) {
-            //Receive Location
+            //Receive Location 如果定位成功需要及时关闭定位,否则耗电太快
+            if(location != null){
+                mLocationClient.stop();
+            }
             StringBuffer sb = new StringBuffer(256);
             sb.append("time : ");
             sb.append(location.getTime());
