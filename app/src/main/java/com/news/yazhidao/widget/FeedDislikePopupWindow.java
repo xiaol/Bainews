1b3b8bd1d07794b3f45f41fc1bf9a8b11c13a0bf
feng <fly_elephant@163.com>
2016-04-06 10:14:33 +0800

    add dislike step2

--- app/src/main/java/com/news/yazhidao/widget/FeedDislikePopupWindow.java
+++ app/src/main/java/com/news/yazhidao/widget/FeedDislikePopupWindow.java
@@ -1,6 +1,8 @@
 package com.news.yazhidao.widget;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.PopupWindow;
@@ -19,5 +21,11 @@ public class FeedDislikePopupWindow extends PopupWindow {
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         rootView = inflater.inflate(R.layout.popup_dislike, null);
         setContentView(rootView);
+        this.setWidth(500);
+        this.setHeight(300);
+        this.setFocusable(true);
+        //实例化一个ColorDrawable颜色为半透明
+        ColorDrawable dw = new ColorDrawable(Color.parseColor("#50b5eb"));
+        this.setBackgroundDrawable(dw);
     }
 }
